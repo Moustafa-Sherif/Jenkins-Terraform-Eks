9031e5a58d6bb73dcea7e384de46e9d760111dba
moustafa.sherif <moustafa.sherif@widebot.net>
2024-02-15 17:01:53 +0200

    updated the dockerImage

--- script.groovy
+++ script.groovy
@@ -15,7 +15,7 @@ def buildImage() {
 
 def pushImage() {
     echo "Push Docker Image to ECR"
-    def dockerImage = docker.build(imageRegUrl + ":$BUILD_NUMBER", "./Docker-files/app/multistage/")
+    def dockerImage = docker.build("jenkins-pipeline:$BUILD_NUMBER", "./Docker-files/app/multistage/")
     docker.withRegistry(imageRegUrl, 'ecr:us-east-2:AWS_Credentials') {
         dockerImage.push("$BUILD_NUMBER")
         dockerImage.push('latest')
